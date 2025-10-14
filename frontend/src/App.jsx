383a50d2f8d9a9742fb50d8d0b4f9d3f0a58263c
LAHARIKA MATETI <145182165+matetilaharika2003@users.noreply.github.com>
2025-10-14 17:15:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import io from "socket.io-client";
 import Editor from "@monaco-editor/react";
 import { v4 as uuid } from "uuid";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://realtimecodeeditor-1-871k.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
